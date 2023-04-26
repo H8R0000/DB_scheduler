c89f01962270afc8a490c33018299d4d69439b25
Kearnut <83284575+IvanKozhukhovsky@users.noreply.github.com>
2023-04-26 23:05:57 +0300

    Release version
    output tables done

--- Vizualizer/src/Main/PrintSchedule.py
+++ Vizualizer/src/Main/PrintSchedule.py
@@ -11,6 +11,11 @@ from PyQt6.QtWidgets import (
    QDialog
 )
 import os
+import json 
+import pandas as pd
+import time
+
+
 
 class Schedule(QDialog):
    def __init__(self):
@@ -34,6 +39,7 @@ class Schedule(QDialog):
       )
       buttons.accepted.connect(self.accept)
       buttons.accepted.connect(self.receive_date)
+      buttons.accepted.connect(self.print_schedule)
       buttons.rejected.connect(self.reject)
 
       layout.addWidget(buttons)
@@ -42,4 +48,66 @@ class Schedule(QDialog):
 
    def receive_date(self):
       time = str(self.date.date().toPyDate())
-      os.system("../test/a.out " + time)
+      print(".\scheduler.exe -d " + time + " > schedule.json")
+      os.system(".\scheduler.exe -d " + time + " > schedule.json")
+   
+   def print_schedule(self):
+
+      while not os.path.exists("./schedule.json"):
+         time.sleep(0.5)
+      
+      def align_center(x):
+         return ['text-align: center' for x in x]
+
+      writer = pd.ExcelWriter('../data/schedule.xlsx', engine='xlsxwriter')
+      with open("./schedule.json", "rb") as f:
+         file = json.load(f) 
+
+      for i in range(len(file)):
+         teacher = file[i]["teacher"]
+
+         info_date_first = ["Время занятий: 9:25"]
+         info_date_second = ["Время занятий: 13:25"]
+         info_students_first_shift = []
+         info_students_second_shift = []
+         for index in range(len(file[i]["schedule"][0]["firstShift"])):
+            info_students_first_shift.append(file[i]["schedule"][0]["firstShift"][index]["student"])
+         for index in range(len(file[i]["schedule"][0]["secondShift"])):
+            info_students_second_shift.append(file[i]["schedule"][0]["secondShift"][index]["student"])
+
+         info_labs_first_shift = []
+         info_labs_second_shift = []
+         info_labs_first_shift_massive = [info_students_first_shift]
+         info_labs_second_shift_massive = [info_students_second_shift]
+         for info in file[i]["schedule"]:
+            info_date_first.append(info["date"])
+            info_date_second.append(info["date"])
+            for index in range(len(info["firstShift"])):
+               info_labs_first_shift.append(info["firstShift"][index]["laba"])
+            for index in range(len(info["secondShift"])):
+               info_labs_second_shift.append(info["secondShift"][index]["laba"])
+            info_labs_first_shift_massive.append(info_labs_first_shift)
+            info_labs_second_shift_massive.append(info_labs_second_shift)
+      
+            info_labs_first_shift = []
+            info_labs_second_shift = []
+
+         sched_first = dict(zip(info_date_first, info_labs_first_shift_massive))
+         sched_second = dict(zip(info_date_second, info_labs_second_shift_massive))
+
+         dt_first = pd.DataFrame(sched_first)
+         dt_second = pd.DataFrame(sched_second)
+
+         info_sheets = dict(zip([teacher], [dt_first]))
+         for sheet_name in info_sheets.keys():
+               info_sheets[sheet_name].style.apply(align_center, axis=0).to_excel(writer, sheet_name=sheet_name, index=False)
+         info_sheets = dict(zip([teacher], [dt_second]))
+         for sheet_name in info_sheets.keys():
+               info_sheets[sheet_name].style.apply(align_center, axis=0).to_excel(writer, sheet_name=sheet_name, index=False, startrow= (len(info_students_first_shift)+2))
+   
+         for i in range(len(info_date_first)):
+            writer.sheets[teacher].set_column(i, i, 30)
+
+      writer.save()
+
+
--- Vizualizer/test/test.py
+++ Vizualizer/test/test.py
@@ -1,35 +1,60 @@
 #import os
+import json 
+import pandas as pd
+
 #os.system("./a.out 12")
 
+def align_center(x):
+    return ['text-align: center' for x in x]
 
-## импортируем библиотеку
-#import json
-## объявляем переменные
-#string = "Some test string"
-#integer = 211
-#array = [1, 2, 3, 4, 5]
-## создаем словарь
-#mydict = {"title": string, "code": integer, "data": array}
-## сериализуем его в JSON-структуру, как строку
-#x = json.dumps(mydict)
-## проводим десериализацию JSON-объекта
-#y = json.loads(x)
+writer = pd.ExcelWriter('../data/schedule.xlsx', engine='xlsxwriter')
+with open("file.json", "rb") as f:
+   file = json.load(f) 
 
+for i in range(len(file)):
+   teacher = file[i]["teacher"]
 
-import json # импортируем библиотеку
+   info_date_first = ["Время занятий: 9:25"]
+   info_date_second = ["Время занятий: 13:25"]
+   info_students_first_shift = []
+   info_students_second_shift = []
+   for index in range(len(file[i]["schedule"][0]["firstShift"])):
+      info_students_first_shift.append(file[i]["schedule"][0]["firstShift"][index]["student"])
+   for index in range(len(file[i]["schedule"][0]["secondShift"])):
+      info_students_second_shift.append(file[i]["schedule"][0]["secondShift"][index]["student"])
 
-## создаем filehandler с помощью контекстного менеджера
-#with open("data.json", "w") as fh:
-    #json.dump([1, 2, 3, 4, 5], fh) # записываем структуру в файл
+   info_labs_first_shift = []
+   info_labs_second_shift = []
+   info_labs_first_shift_massive = [info_students_first_shift]
+   info_labs_second_shift_massive = [info_students_second_shift]
+   for info in file[i]["schedule"]:
+      info_date_first.append(info["date"])
+      info_date_second.append(info["date"])
+      for index in range(len(info["firstShift"])):
+         info_labs_first_shift.append(info["firstShift"][index]["laba"])
+      for index in range(len(info["secondShift"])):
+         info_labs_second_shift.append(info["secondShift"][index]["laba"])
+      info_labs_first_shift_massive.append(info_labs_first_shift)
+      info_labs_second_shift_massive.append(info_labs_second_shift)
+      
+      info_labs_first_shift = []
+      info_labs_second_shift = []
 
+   sched_first = dict(zip(info_date_first, info_labs_first_shift_massive))
+   sched_second = dict(zip(info_date_second, info_labs_second_shift_massive))
 
-# открываем тот же файл, но уже на чтение
-with open("schedule.json", "rb") as f:
-   dict = json.load(f) # загружаем структуру из файла
+   dt_first = pd.DataFrame(sched_first)
+   dt_second = pd.DataFrame(sched_second)
 
-count = len(dict[0]["schedule"])
-info_students = []
-for info_student in dict[0]["schedule"]:
-   info_students.append(info_student)
+   info_sheets = dict(zip([teacher], [dt_first]))
+   for sheet_name in info_sheets.keys():
+         info_sheets[sheet_name].style.apply(align_center, axis=0).to_excel(writer, sheet_name=sheet_name, index=False)
+   info_sheets = dict(zip([teacher], [dt_second]))
+   for sheet_name in info_sheets.keys():
+         info_sheets[sheet_name].style.apply(align_center, axis=0).to_excel(writer, sheet_name=sheet_name, index=False, startrow= (len(info_students_first_shift)+2))
+   
+   for i in range(len(info_date_first)):
+      writer.sheets[teacher].set_column(i, i, 30)
 
-print(info_students[0])
+writer.save()
+
